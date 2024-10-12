089b322cd834db24ee4398bcc528c42535dfd451
bcmlily <cahyabriliana@gmail.com>
2024-10-12 21:27:09 +0700

    add nav bar and side bar

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,68 +1,91 @@
 const { description } = require('../../package')
 
 module.exports = {
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#title
-   */
-  title: 'Vuepress Docs Boilerplate',
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#description
-   */
+  title: 'SIPKK',
   description: description,
 
-  /**
-   * Extra tags to be injected to the page HTML `<head>`
-   *
-   * ref：https://v1.vuepress.vuejs.org/config/#head
-   */
   head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['link', { rel: 'icon', href: `/assets/img/favicon.png` }],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto+Mono|Montserrat&display=swap' }],
+    ['meta', { name: 'theme-color', content: '#005FAC' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
 
-  /**
-   * Theme configuration, here is the default theme configuration for VuePress.
-   *
-   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-   */
+  theme: "book",
   themeConfig: {
+    enableDarkMode: false,
+    logo: '/gambar/logo.jpeg',
+    locales: {
+      '/': {
+        lang: 'en-US',
+      }
+    },
     repo: '',
     editLinks: false,
     docsDir: '',
     editLinkText: '',
     lastUpdated: false,
+    nextLinks: false,
+    prevLinks: false,
+    search: true,
+    searchPlaceholder: 'Pencarian...',
+    smoothScroll: true,
     nav: [
       {
-        text: 'Guide',
-        link: '/guide/',
+        text: 'Home',
+        link: '/index',
       },
       {
-        text: 'Config',
+        text: 'Daftar Isi',
         link: '/config/'
       },
       {
-        text: 'VuePress',
+        text: 'Dokumentasi',
+        link: '/docs/introduction/' // Link ke introduction
+      },
+      {
+        text: 'Aplikasi SIPKK V.4',
         link: 'https://v1.vuepress.vuejs.org'
       }
     ],
     sidebar: {
-      '/guide/': [
+      '/': [
         {
-          title: 'Guide',
+          title: "",
           collapsable: false,
+          sidebarDepth: 1,
+          children: [["/", "Home"]]
+        },
+        {
+          title: 'Getting Started',
+          collapsable: true,
           children: [
-            '',
-            'using-vue',
+            '/guide/introduction', // Contoh file panduan
+            '/guide/installation', // Contoh file instalasi
+          ]
+        },
+        {
+          title: 'Features',
+          collapsable: true,
+          children: [
+            '/features/feature1',
+            '/features/feature2',
           ]
         }
       ],
+      '/docs/': [
+        {
+          title: 'Documentation',
+          collapsable: false,
+          children: [
+            'introduction', // Tambahkan entry ini
+            'setup', // File setup tambahan
+          ]
+        }
+      ]
     }
   },
-
-  /**
-   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-   */
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
